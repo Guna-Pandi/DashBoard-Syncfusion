7fc6c73d3ab5ebe0e73e6e3bbe1a9c8e34e3ecf8
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-05-19 10:48:24 +0530

    Added:spark line datra

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import {
-  Sparkline,
   Inject,
   SparklineTooltip,
+  SparklineComponent,
 } from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      dataSource={data}
+      xName="x"
+      yName="y"
+      type={type}
+      fill={color}
+      border={{ color: currentColor, width: 2 }}>
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+  );
 };
 
 export default SparkLine;
