9b4cfc8091ea93a392e41c79a9be653388958d20
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-05-19 09:51:13 +0530

    Added:Sparkline area

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import {
+  Sparkline,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
 const SparkLine = () => {
-  return (
-    <div>SparkLine</div>
-  )
-}
+  return <div>SparkLine</div>;
+};
 
-export default SparkLine
+export default SparkLine;
