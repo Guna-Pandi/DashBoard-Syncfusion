0c976b7d8b5e31324a3bd302ff40dfbeccec37a6
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-05-20 23:02:40 +0530

    Fix:ContextProvider Added:Notofication

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,56 +1,40 @@
-import { UsedRange } from "@syncfusion/ej2/spreadsheet";
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState } from 'react';
+
 const StateContext = createContext();
+
 const initialState = {
   chat: false,
   cart: false,
   userProfile: false,
   notification: false,
 };
+
 export const ContextProvider = ({ children }) => {
-  const [activeMenu, setActiveMenu] = useState("true");
-  const [isClicked, setisClicked] = useState(initialState);
   const [screenSize, setScreenSize] = useState(undefined);
-  const [currentColor, setCurrentColor] = useState("#03C9D7");
-  const [currentMode, setCurrentMode] = useState("Light");
-  const [themeSettings, setThemeSettings] = useState("false");
+  const [currentColor, setCurrentColor] = useState('#03C9D7');
+  const [currentMode, setCurrentMode] = useState('Light');
+  const [themeSettings, setThemeSettings] = useState(false);
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
 
   const setMode = (e) => {
     setCurrentMode(e.target.value);
-    localStorage.setItem("themeMode", e.target.value);
-    setThemeSettings(false);
+    localStorage.setItem('themeMode', e.target.value);
   };
 
   const setColor = (color) => {
     setCurrentColor(color);
-    localStorage.setItem("colorMode", color);
-    setThemeSettings(false);
+    localStorage.setItem('colorMode', color);
   };
 
-  const handleClick = (clicked) => {
-    setisClicked({ ...initialState, [clicked]: true });
-  };
+  const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+
   return (
-    <StateContext.Provider
-      value={{
-        activeMenu,
-        setActiveMenu,
-        isClicked,
-        setisClicked,
-        handleClick,
-        screenSize,
-        setScreenSize,
-        currentColor,
-        currentMode,
-        // setCurrentColor,
-        // setCurrentMode,
-        themeSettings,
-        setThemeSettings,
-        setColor,
-        setMode
-      }}>
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
       {children}
     </StateContext.Provider>
   );
 };
+
 export const useStateContext = () => useContext(StateContext);
